82f9745f2dc00c224a3a757aa74068a910c3d9ef
Umair Shehzad <umairshehzad247365@gmail.com>
2023-04-11 09:33:26 +0500

    remove computers 3D model

--- src/components/index.js
+++ src/components/index.js
@@ -1,18 +1,13 @@
-import {
-  EarthCanvas,
-  BallCanvas,
-  ComputersCanvas,
-  StarsCanvas,
-} from "./canvas";
-import Hero from "./Hero";
-import Navbar from "./Navbar";
-import About from "./About";
-import Tech from "./Tech";
-import Experience from "./Experience";
-import Works from "./Works";
+import { EarthCanvas, BallCanvas, StarsCanvas } from './canvas'
+import Hero from './Hero'
+import Navbar from './Navbar'
+import About from './About'
+import Tech from './Tech'
+import Experience from './Experience'
+import Works from './Works'
 // import Feedbacks from "./Feedbacks";
-import Contact from "./Contact";
-import CanvasLoader from "./Loader";
+import Contact from './Contact'
+import CanvasLoader from './Loader'
 
 export {
   Hero,
@@ -26,6 +21,5 @@ export {
   CanvasLoader,
   EarthCanvas,
   BallCanvas,
-  ComputersCanvas,
   StarsCanvas,
-};
+}
